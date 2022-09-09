25250be2385aaebd23f44a1ed7269cd46b1260bb
leoneidas <leonearina@gmail.com>
2022-09-08 17:03:31 -0700

    second bunch

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#!/bin/bash
+/**
+ * main - Entry point
+ *
+ * Return: 0 after printing the function
+ */
+int main(void)
+{puts("\"Programming is like building a multilingual puzzle");
+	return (0)
+}
